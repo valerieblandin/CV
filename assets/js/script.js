7421dce249edf435c2550e8974870572979cc1ca
valerieblandin <valerie.blandin@yahoo.fr>
2021-10-15 15:15:43 +0200

    online

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+function myFunction() {
+    document.getElementById('menu').classList.toggle('responsive');
+}
+
+const animScroll = document.getElementsByClassName('tetrisIcones');
+
+window.addEventListener('scroll', function(){
+    scrollValue = (window.innerHeight + window.scrollY) / (document.body.offsetHeight);
+
+    if (scrollValue > 0.8){
+        animScroll.style.animationPlayState = 'running';
+    } else{
+        animScroll.style.animationPlayState = 'paused';
+    }
+});
+
